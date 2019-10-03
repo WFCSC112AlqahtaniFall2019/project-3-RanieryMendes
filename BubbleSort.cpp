4131aec2746a26476192e4a589221c6d17794024
ranierymendes <mendrc18@wfu.edu>
2019-10-03 03:38:05 -0400

    Commit #4 Program works properly. Final Comments added

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -16,17 +16,17 @@ void bubbleSort(int *a, int n);
 
 int main(int argc, char* argv[]) {
 
+    // File to print output to plot Time vs MergeSort and BubbleSort's elapsed time
     ofstream outFS;
-
     outFS.open("output.txt",ios::app);
-
-
-
+    //Check if file was opened
     if (!outFS.is_open()) {
         cout << "Could not open file myoutfile.txt." << endl;
         return 1;
     }
 
+
+    //Check if user typed the right number of arguments.
     if (argc != 3) {
         cout << "Properly Usage: ./BubbleSort seed length" << endl;
         return 1; // 1 indicates error
@@ -34,8 +34,8 @@ int main(int argc, char* argv[]) {
 
     // get input: first is random seed, second is vector length
     int seed, length;
-     seed = atoi(argv[1]);
-     length =atoi(argv[2]);
+    seed = atoi(argv[1]);
+    length =atoi(argv[2]);
 
 
     srand(seed);
@@ -63,14 +63,19 @@ int main(int argc, char* argv[]) {
    }
 
    // unit test for Swap function
-   int* test = new int [2];
+   int* test = new int [2]; // Create array to test Swap function
+
+   //define two elements in descending order to be swapped
    test [0] = 90;
    test [1] = 53;
+   //Call Swap function
    swap(test[0], test[1]);
+   //Check if it worked
    assert(test[0] < test[1]);
+   delete test; //deallocate memory for Array test
 
 
-
+   //collect mergeSort's elapsed time
     clock_t start_mergeSort = clock();
     // sort vector using mergeSort
     mergeSort(v,t, 0, v.size() - 1);
@@ -82,21 +87,24 @@ int main(int argc, char* argv[]) {
     }
 
     //Unit Test for BubbleSort function
-    int* bubblesort_test = new int [12];
+    int* bubblesort_test = new int [12]; //create array for test bubbleSort function
 
+    //allocate random number as elements of the array. They are allocated in random order.
     for (int p = 0; p < 12; p++){
-        bubblesort_test[p] = rand()%100;
+        bubblesort_test[p] = rand()%50; //
     }
 
-    bubbleSort(bubblesort_test, 12); // call bubblesort to be tested bellow
+    bubbleSort(bubblesort_test, 12); // call bubbleSort to be tested bellow
 
+    // check if array is sorted and consequently if BubbleSort works
     for (int m = 1; m < 12 ; ++m) {
-        assert(bubblesort_test[m-1] < bubblesort_test[m]);
+        assert(bubblesort_test[m-1] <= bubblesort_test[m]);
     }
 
+    delete bubblesort_test;  //deallocate memory for this array
 
 
-
+    //collect bubbleSort's elapsed time
     clock_t start_bubbleSort = clock();
 
     // sort array using bubbleSort
@@ -129,14 +137,14 @@ int main(int argc, char* argv[]) {
     // print elapsed time for mergeSort and bubbleSort
     double elapsed_mergeSort = double(end_mergeSort - start_mergeSort) / CLOCKS_PER_SEC;
     double elapsed_bubbleSort = double(end_bubbleSort - start_bubbleSort) / CLOCKS_PER_SEC;
-
+    cout << elapsed_mergeSort << "       " << elapsed_bubbleSort << endl;
+    //Print Array's length and mergeSort and bubbleSort's elapsed times into file to plot Time Vs Array's length
     outFS << "Test using length: " << length << endl;
     outFS << elapsed_mergeSort << "       " << elapsed_bubbleSort << endl;
+    outFS.close(); // close file
 
-    outFS.close();
 
-    cout << elapsed_mergeSort << "       " << elapsed_bubbleSort << endl;
-    delete ptr;
+    delete ptr; //deallocate memory of the dynamic array ptr
 
 
 
@@ -169,6 +177,7 @@ void mergeSortedLists(vector<int>& a, vector<int>& tmp, int leftPos, int rightPo
     }
 }
 
+//MergeSort function
 void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right) {
     if (left < right) {
         int center = ( left + right ) / 2;
